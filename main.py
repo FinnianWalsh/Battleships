e3ce036b0888d1b3703ff3631f9195c0b22c957b
FinnianWalsh <finnian.walsh@edgbarroschool.co.uk>
2025-05-10 15:01:16 +0100

    Implement line counter

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-from app.game import Game
+from app import Game
+
+import os
 
 if __name__ == "__main__":
+    print(os.getcwd())
+    total = 0
+
+    for root, dirs, files in os.walk(f"{os.getcwd()}/app"):
+        for fileName in files:
+            if not fileName.endswith(".py"):
+                continue
+            with open(os.path.join(root, fileName), "r") as fileInstance:
+                while fileInstance.readline():
+                    total += 1
+
+    print(f"Total app lines: {total}")
+
     game = Game()
     game.start()

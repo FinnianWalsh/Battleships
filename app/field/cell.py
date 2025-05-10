3424ed53452fdbff20ed2ef1a9fbd798fc18ff1b
FinnianWalsh <finnian.walsh@edgbarroschool.co.uk>
2025-05-10 14:59:02 +0100

    Extend Cell class

--- app/field/cell.py
+++ app/field/cell.py
@@ -1,9 +1,7 @@
 import tkinter as tk
-
-from typing import Any, Optional, Self
+from typing import Any, Tuple
 
 from ..util import OptSelf
-
 
 class NullCell:
     _instance = None
@@ -21,19 +19,20 @@ class NullCell:
         return "NullCell()"
 
 
-
 NullCell = NullCell()
 
 
 class Cell:
-    def __init__(self, label: tk.Label, position: str, above: OptSelf = None, below: OptSelf = None,
-                 left: OptSelf = None, right: OptSelf = None):
+    def __init__(self, label: tk.Label, position_vector: Tuple[str, int], above: OptSelf = None,
+                 below: OptSelf = None, left: OptSelf = None, right: OptSelf = None):
         self.label = label
-        self.position = position
+        self.position_vector = position_vector
+        self.position = f"{position_vector[0]}{position_vector[1]}"
         self.above = above
         self.below = below
         self.left = left
         self.right = right
+        self.ship = None
 
     def __setattr__(self, name: str, value: Any):
         if getattr(self, name, None) is not None:
@@ -45,3 +44,6 @@ class Cell:
 
     def __repr__(self):
         return f"Cell(label={self.label}, position={self.position}, above={self.above}, below={self.below}, left={self.left}, right={self.right})"
+
+    def reset(self):
+        object.__setattr__(self, "ship", None)

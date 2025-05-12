a761a84b0d9d2c9420fe7cff33a21b7bdc39249c
FinnianWalsh <finnian.walsh@edgbarroschool.co.uk>
2025-05-12 19:47:26 +0100

    Tidy line counter

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from app import Game
 import os
 
 if __name__ == "__main__":
-    print(os.getcwd())
     total = 0
+    cwd = os.getcwd()
 
-    for root, dirs, files in os.walk(f"{os.getcwd()}/app"):
+    for root, dirs, files in os.walk(f"{cwd}/app"):
         for fileName in files:
             if not fileName.endswith(".py"):
                 continue
@@ -14,7 +14,7 @@ if __name__ == "__main__":
                 while fileInstance.readline():
                     total += 1
 
-    print(f"Total app lines: {total}")
+    print(f"Total app lines in: {total}")
 
     game = Game()
     game.start()

32211294e94928b26f4fe7702fc43e454abadba3
FinnianWalsh <finnian.walsh@edgbarroschool.co.uk>
2025-05-12 19:46:59 +0100

    Tidy code and update grid consts

--- app/constants/grid.py
+++ app/constants/grid.py
@@ -1,7 +1,7 @@
 from ..util import hex_color_mut
 from .root import ROOT_BACKGROUND_COLOR
 
-GRID_X, GRID_Y = 10, 12
+GRID_X, GRID_Y = 10, 10
 
 assert GRID_X >= 2, "GRID_X must be at least 2!"
 assert GRID_Y >= 2, "GRID_Y must be at least 2!"
@@ -12,3 +12,5 @@ RELATIVE_ROTATION_LABEL_SIZE = 0.1
 RELATIVE_TURN_LABEL_HEIGHT = 0.1
 
 GRID_COLOR = hex_color_mut(ROOT_BACKGROUND_COLOR, lambda n: n * 1.15)
+
+CELL_DESTROY_COLOR = hex_color_mut(GRID_COLOR, lambda n: n * 0.75)

014e067f5c9f2380d085f6fa90487bc24ef3eef2
FinnianWalsh <finnian.walsh@edgbarroschool.co.uk>
2025-05-12 19:47:56 +0100

    Update misc consts

--- app/constants/misc.py
+++ app/constants/misc.py
@@ -1,3 +1,22 @@
+from enum import auto, Enum
+
 ALPHA_BASE = ord('A') - 1
 
 MAX_NAME_LENGTH = 10
+
+FILL_SYMBOLS = {
+    "above": '↑',
+    "right": '→',
+    "below": '↓',
+    "left": '←',
+}
+
+
+class SetupSize(Enum):
+    MINIMAL = auto()
+    SMALL = auto()
+    NORMAL = auto()
+    LARGE = auto()
+
+
+SHIP_SETUP_SIZE = SetupSize.NORMAL
